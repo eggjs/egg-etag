bd744c032861550ec5c1d734ae8305a63f001309
TZ <atian25@qq.com>
2017-01-28 18:15:15 +0800

    fix: adjust with new test

--- test/etag.test.js
+++ test/etag.test.js
@@ -2,14 +2,12 @@
 
 const request = require('supertest');
 const mm = require('egg-mock');
-const path = require('path');
 
 describe('test/etag.test.js', () => {
   let app;
   before(() => {
     app = mm.app({
       baseDir: 'apps/etag',
-      customEgg: path.join(__dirname, '../node_modules/egg'),
     });
     return app.ready();
   });
